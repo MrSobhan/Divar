d85882fecbb8e1fa1e1c08b81bb72fa5b7c69b81
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-10-19 20:14:12 +0330

    DarkMode Handler UserPanel All pages :))

--- src/Components/HeaderDefault/HeaderDefault.jsx
+++ src/Components/HeaderDefault/HeaderDefault.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React , {useState , useContext , useEffect} from 'react';
 import '../HeaderMain/HeaderMain.css'
 import { useNavigate, Link } from 'react-router-dom';
-
+import AuthContext from '../../context/authContext';
 const HeaderDefault = () => {
     const navigetor = useNavigate()
+    const authContext = useContext(AuthContext)
+    const [theme, setTheme] = useState(authContext.getLocalStorage('theme'))
+
+    useEffect(() => {
+        if (theme == 'light') {
+            document.documentElement.style.setProperty('--white-color', '#f4f4f4');
+            document.documentElement.style.setProperty('--text-color', 'rgba(0, 0, 0, 0.56)');
+            document.documentElement.style.setProperty('--black-color', '#242424');
+            document.documentElement.style.setProperty('--border', '1px solid rgba(0, 0, 0, 0.2)');
+        } else {
+            document.documentElement.style.setProperty('--white-color', '#242424');
+            document.documentElement.style.setProperty('--text-color', 'rgba(255, 255, 255, 0.56)');
+            document.documentElement.style.setProperty('--black-color', '#fff');
+            document.documentElement.style.setProperty('--border', '1px solid rgba(255, 255, 255, 0.2)');
+        }
+    }, [theme])
     return (
         <header className="header">
             <div className="container-fluid">
